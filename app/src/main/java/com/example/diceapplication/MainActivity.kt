d6346b419d14e8d7c74a642a35eadfe8c5288ab9
Domex99 <rave.monkey36@gmail.com>
2021-03-02 16:04:32 +0100

    Basic version fully working

--- app/src/main/java/com/example/diceapplication/MainActivity.kt
+++ app/src/main/java/com/example/diceapplication/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.diceapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val mGenerator = Random()
+
+    val diceIds = arrayOf(0, R.drawable.dice1, R.drawable.dice2, R.drawable.dice3,
+                             R.drawable.dice4,  R.drawable.dice5, R.drawable.dice6
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickRoll(view: View) {
+        val d1 = mGenerator.nextInt(6)+1
+        val d2 = mGenerator.nextInt(6)+1
+
+        imgDice1.setImageResource(diceIds[d1])
+        imgDice2.setImageResource(diceIds[d2])
+
     }
 }
